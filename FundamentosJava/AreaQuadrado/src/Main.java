64bb5ebb3c704d4ef69a44bfdd57a0eca8acdc3f
PedroValenteIHS <pedrohvalentepaulino@gmail.com>
2026-04-13 14:18:20 -0300

    Adiconando area de quadrado e retangulo

--- FundamentosJava/AreaQuadrado/src/Main.java
+++ FundamentosJava/AreaQuadrado/src/Main.java
@@ -1,19 +1,29 @@
-// Escreva um código que receba os valores da altura e largura de um quadrado
-// e retorne a área.
+// Escreva um código que receba os valores da altura e largura de um retangulo
+// e retorne a área. O mesmo para um quadrado.
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //quadrado
         // recebe os valores da altura e largura
         var scanner = new Scanner(System.in);
         System.out.println("Informe o valor da largura em cm do quadrado");
         float largura = scanner.nextFloat();
-        System.out.println("Informe o valor da altura em cm do quadrado");
+        // calcula a área e retorna o valor
+        float area = largura * largura;
+        System.out.printf("A área do quadrado informado é %s cm", area);
+
+        //retangulo
+        // recebe os valores da altura e largura
+        System.out.println("Informe o valor da largura em cm do retangulo");
+        largura = scanner.nextFloat();
+        System.out.println("Informe o valor da altura em cm do retangulo");
         float altura = scanner.nextFloat();
         // calcula a área e retorna o valor
-        float area = largura * altura;
-        System.out.printf("A área do quadrado informado é %s cm", area);
+        area = largura * altura;
+        System.out.printf("A área do retangulo informado é %s cm", area);
+
     }
 }
 
