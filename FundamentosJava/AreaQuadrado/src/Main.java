7d4ec08e7251a8f7ede57d7d74f5b4d8da6054e5
PedroValenteIHS <pedrohvalentepaulino@gmail.com>
2026-04-13 10:10:33 -0300

    Add files via upload

---
+++ FundamentosJava/AreaQuadrado/src/Main.java
@@ -0,0 +1,18 @@
+// Escreva um código que receba os valores da altura e largura de um quadrado
+// e retorne a área.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // recebe os valores da altura e largura
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe o valor da largura em cm do quadrado");
+        float largura = scanner.nextFloat();
+        System.out.println("Informe o valor da altura em cm do quadrado");
+        float altura = scanner.nextFloat();
+        // calcula a área e retorna o valor
+        float area = largura * altura;
+        System.out.printf("A área do quadrado informado é %s cm", area);
+    }
+}
